7b4a38b1761c865deb691fb7eac6b62da588cd9c
thanhtu02 <hanhee1406@gmail.com>
2024-01-20 14:34:38 +0700

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import DetailPage from './pages/Detail';
 import MarketPage from './pages/Market';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import data from '../api/bctc/data.json'
+import data from './api/bctc/data.json'
 import { DataContext } from './context/dataContext';
 
 function App() {
